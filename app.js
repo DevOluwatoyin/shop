e3cba37565110a1ca431fc79176ebc73187cba8e
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2023-11-25 22:12:29 +0100

    completed styling for accordion... woring on login

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const notification = document.querySelector("#bell");
 const alert = document.querySelector("#notify");
+const menuButton = document.querySelector("#user");
+const menuDropdown = document.querySelector("#menu-dropdown");
+const setupControl = document.querySelector("#toggle-guides");
+const setupSteps = document.querySelector("#steps");
+const foldSetupSteps = document.querySelector("#close-steps");
+const openSetupSteps = document.querySelector("#open-steps");
+const trialCallout = document.querySelector("#trial-callout");
+const dismissBtn = document.querySelector("#dismiss");
 
 function openAlert() {
   alert.classList.toggle("hide");
@@ -13,9 +21,34 @@ function openAlert() {
   alertFocus.focus();
 }
 
+function openMenu() {
+  menuDropdown.classList.toggle("hide");
+  const isExpanded = menuButton.getAttribute("aria-expanded") === "true";
+
+  const menuItems = menuDropdown.querySelectorAll("[role='menuitem']");
+
+  menuItems.item(0).focus();
+
+  isExpanded
+    ? menuButton.setAttribute("aria-expanded", "false")
+    : menuButton.setAttribute("aria-expanded", "true");
+}
+
+function openSetupGuide() {
+  openSetupSteps.classList.toggle("hide");
+  setupSteps.classList.toggle("hide");
+  foldSetupSteps.classList.toggle("hide");
+}
+
+function dismissTrialCallout() {
+  trialCallout.classList.add("hide");
+}
 
 function togglePopup() {
   notification.addEventListener("click", openAlert);
+  menuButton.addEventListener("click", openMenu);
+  dismissBtn.addEventListener("click", dismissTrialCallout);
+  setupControl.addEventListener("click", openSetupGuide);
 }
 
 togglePopup();
